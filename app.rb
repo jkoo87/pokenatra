4d9ba7730a49c87c49992d4f786f3394d1ba2d71
Jeong Hoon Koo <junghoone17@gmail.com>
2017-04-18 13:04:58 -0400

    MVP complete, but unable to attach pokemons to a trainer

--- app.rb
+++ app.rb
@@ -17,6 +17,26 @@ get '/trainers' do
   erb :"trainers/index"
 end
 
+get '/trainers/trainer:id/catch_pokemon' do
+  @pokemon = Pokemon.all
+  @trainer = Trainer.find(params[:id])
+  erb :"trainers/catch"
+end
+
+# put '/trainers/:id' do
+#   @trainer = Trainer.create
+#
+# end
+
+
+
+
+
+
+
+
+
+
 get '/trainers/:id' do
   @trainer = Trainer.find(params[:id])
   erb :"trainers/show"
@@ -26,14 +46,23 @@ end
 
 
 
-get '/new' do
+
+
+get '/pokemons/new' do
   @pokemon = Pokemon.all
   erb :"pokemons/new"
 end
 
+
 post '/pokemons' do
   @pokemons = Pokemon.create(name: params[:name], cp: params[:cp], poke_type: params[:poke_type], img_url: params[:img_url])
   redirect "/pokemons/view?id=#{@pokemons.id}"
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemons/view?id=#{@pokemon.id}")
 end
 
 get '/pokemons' do
@@ -45,4 +74,17 @@ end
 get '/pokemons/view' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
+end
+
+get '/pokemons/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+
+
+delete '/pokemons/:id/delete' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect('/pokemons')
 end
